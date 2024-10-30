847ed75c6e2227e1fd32b7eaec2c1e0f5b3dd166
Phillip H <phillip.hickinbotham@sunderlandsoftwarecity.com>
2024-10-30 14:55:16 +0000

    initial plant commit

---
+++ plants/index.mjs
@@ -0,0 +1,27 @@
+import cors from 'cors';
+import 'dotenv/config';
+import express from 'express';
+import nunjucks from 'nunjucks';
+import api from './routes/api.mjs';
+import web from './routes/web.mjs';
+
+const app = express();
+app.use(cors());
+app.use(express.static('public'));
+
+app.use('/api', api); // for services
+app.use('/', web); // for students
+
+nunjucks.configure('views', {
+	autoescape: true,
+	express: app,
+	watch: true,
+});
+
+app.set('view engine', 'njk');
+
+const APP_PORT = process.env.APP_PORT ?? 3000;
+
+app.listen(APP_PORT, () => {
+	console.log(`Server running on http://localhost:${APP_PORT}`);
+});
---
+++ plants/routes/web.mjs
@@ -0,0 +1,10 @@
+import express from 'express';
+// ** import funcs **
+
+const appRoutes = express.Router();
+
+appRoutes.get('/', async (req, res, next) => {
+	// TODO: insert try catch block here
+});
+
+export default appRoutes;
